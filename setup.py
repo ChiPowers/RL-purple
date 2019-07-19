e39e776eecab418b9bb7d52cadbae2af9a5c6c66
Reuben Brasher <rkbrasher@gmail.com>
2019-07-19 14:54:05 -0700

    Sketch package setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for RL Purple package
+"""
+
+from distutils.core import setup
+
+setup (name     = 'rl_purple',
+    version     = '0.1.0',
+    author      = "Chivon Powers",
+    description = """Reinforcement learning for text comprehension""")
